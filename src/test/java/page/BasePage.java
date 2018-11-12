f58a740ec5503200537d297444df10ede9c3c158
Vladyslav Kalinin <vvkalinin20@gmail.com>
2018-11-12 21:52:33 +0200

    ClassWork-08.11.2018

--- src/test/java/page/BasePage.java
+++ src/test/java/page/BasePage.java
@@ -1,10 +1,10 @@
 package page;
 
 import org.openqa.selenium.TimeoutException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+        import org.openqa.selenium.WebDriver;
+        import org.openqa.selenium.WebElement;
+        import org.openqa.selenium.support.ui.ExpectedConditions;
+        import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class BasePage {
     /**
